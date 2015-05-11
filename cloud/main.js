f024af3126143556b74df4794fa85093801c84b1
Visalli Media <GinaLuciano@Ginas-MacBook-Pro.local>
2015-05-11 09:48:33 -0400

    Express instance & login template

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,15 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+
+// Load rquired modules.
+
+var express = require('express');
+var querystring = require('querystring');
+var _ = require('underscore');
+var Buffer = require('buffer').Buffer;
+
+
+//Create an express application instance
+var app = express();
